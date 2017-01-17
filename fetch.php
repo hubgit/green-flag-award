92466ac2114d657c7a4ad94033c4f0bdc100f875
Alf Eaton <hubgit@users.noreply.github.com>
2017-01-17 15:29:07 +0000

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -2,73 +2,67 @@
 
 require_once 'vendor/autoload.php';
 
-/**
- *
- */
 class GreenFlagResource extends WebResource\Resource {
-    /**
-     * @return array
-     */
-    protected function parse ($data) {
-        preg_match(
-            '/var locations = (\[\[.+?\]\])/s',
-            $data,
-            $matches
-        );
+  protected function parse($response) {
+    preg_match(
+      '/var locations = (\[\[.+?\]\])/s',
+      $response->getBody(),
+      $matches
+    );
 
-        return json_decode($matches[1], true);
-    }
+    return json_decode($matches[1], true);
+  }
 }
 
 $areas = [
-    'north-east',
-    'yorkshire-humberside',
-    'north-west',
-    'east-midlands',
-    'west-midlands',
-    'east-of-england',
-    'london',
-    'south-west',
-    'south-east',
-    'wales',
-    'scotland',
+  'north-east',
+  'yorkshire-humberside',
+  'north-west',
+  'east-midlands',
+  'west-midlands',
+  'east-of-england',
+  'london',
+  'south-west',
+  'south-east',
+  'wales',
+  'scotland',
 ];
 
 $features = array_reduce($areas, function ($features, $area) {
-    $resource = new GreenFlagResource(sprintf(
-        'http://www.greenflagaward.org.uk/award-winning-sites/%s/',
-        $area
-    ));
+  $resource = new GreenFlagResource(sprintf(
+    'http://www.greenflagaward.org.uk/award-winning-sites/%s/',
+    $area
+  ));
 
-    foreach ($resource->get() as $location) {
-        //print_r($location);
+  foreach ($resource->get() as $location) {
+    //print_r($location);
 
-        list($name, $lng, $lat, $summary, $image, $id) = $location;
+    list($name, $lng, $lat, $summary, $image, $id) = $location;
 
-        if (!in_array($id, ['1988', '1454'])) {
-            $features[] = [
-                'type' => 'Feature',
-                'geometry' => [
-                    'type' => 'Point',
-                    'coordinates' => [
-                        (float) $lat,
-                        (float) $lng
-                    ],
-                ],
-                'properties' => [
-                    'name' => $name,
-                    'image' => 'http://www.greenflagaward.org.uk/data/UploadedDocuments/' . $image,
-                    'url' => 'http://www.greenflagaward.org/park-summary/?ParkID=' . $id,
-                    'summary' => $summary // TODO: extract data from each location's page
-                ],
-            ];
-        }
+    if (!in_array($id, ['1988', '1454'])) {
+      $features[] = [
+        'type' => 'Feature',
+        'geometry' => [
+          'type' => 'Point',
+          'coordinates' => [
+            (float)$lat,
+            (float)$lng
+          ],
+        ],
+        'properties' => [
+          'name' => $name,
+          'image' => 'http://www.greenflagaward.org.uk/data/UploadedDocuments/' . $image,
+          'url' => 'http://www.greenflagaward.org/park-summary/?ParkID=' . $id,
+          'summary' => $summary // TODO: extract data from each location's page
+        ],
+      ];
     }
+  }
 
-    return $features;
+  return $features;
 }, []);
 
 file_put_contents('green-flag-award.json', json_encode([
-    'type' => 'FeatureCollection',
-    'features' => $features,
+  'type' => 'FeatureCollection',
+  'features' => $features,
 ], JSON_PRETTY_PRINT));
